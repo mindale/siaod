ad3c73371cbe02a7731f6d6724a42fad6a7a9fe9
Даниил <83985398+mindale@users.noreply.github.com>
2024-05-07 20:55:40 +0300

    Add files via upload

--- prakt_2/dinam_2.cpp
+++ prakt_2/dinam_2.cpp
@@ -5,7 +5,31 @@ using namespace std;
 
 #define N 3 // Количество уравнений
 
-float radius_Array(float A[N][N], float B[N]) {
+void radius_Array(int num){
+    float x[num];
+    float y[num];
+    for(int i = 0; i < num; i++){
+        for(int j = i+1; j < num; j++){
+            if (x[i] > x[j] or y[i] > y[j]){
+                swap(x[i], x[j]);
+                swap(y[i], y[j]);
+            }
+        }
+    }
+    for(int i = 0; i < num; i++){
+        cout << "Введите x и y:"; cin >> x[i]; cin >> y[i];
+    }
+    float A[N][N] = {
+            {2*x[0], 2*y[0], 1},
+            {2*x[1], 2*y[1], 1},
+            {2*x[2], 2*y[2], 1}
+    };
+    float b0 =-(pow(x[0],2) + pow(y[0],2));
+    float b1 = -(pow(x[1],2)+pow(y[1],2));
+    float b2 = -(pow(x[2],2) + pow(y[2],2));
+
+    float B[N] = {b0, b1, b2};
+
     float X[N]; // Массив для хранения решения
 
     // Прямой ход метода Гаусса
@@ -36,30 +60,21 @@ float radius_Array(float A[N][N], float B[N]) {
     float Xc = -(X[0]);
     float Yc = -(X[1]);
     float R = sqrt(pow(Xc, 2) + pow(Yc,2) - X[2]);
-    return R;
+//    float ans[3] = {Xc,Yc,R};
+//    return Xc, Yc, R;
+    cout << "X и Y центра окружности: "; cout <<  Xc;cout << ":";cout << Yc; cout << endl;
+    cout << "Радиус окружности: "; cout << R;
+
 }
-//
+
+float center_Array(float x, float y, float rad){
+
+}
+
 int main(){
     SetConsoleOutputCP(CP_UTF8);
     int num;
-    float x[num];
-    float y[num];
     cout << "Введите кол-во точек:"; cin >> num;
-    for(int i = 0; i < num; i++){
-        cout << "Введите x и y:"; cin >> x[i]; cin >> y[i];
-    }
-    float A[N][N] = {
-            {2*x[0], 2*y[0], 1},
-            {2*x[1], 2*y[1], 1},
-            {2*x[2], 2*y[2], 1}
-    };
-    float b0 =-(pow(x[0],2) + pow(y[0],2));
-    float b1 = -(pow(x[1],2)+pow(y[1],2));
-    float b2 = -(pow(x[2],2) + pow(y[2],2));
-    float B[N] = {b0, b1, b2};
-
-    float R = radius_Array(A, B);
-    cout << R;
-
-
+    radius_Array(num);
+//    cout << R;
 }
