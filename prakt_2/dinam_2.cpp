bffd021ae83f1ea11d95b80938ad2e43ed1ddbe0
Даниил <83985398+mindale@users.noreply.github.com>
2024-05-11 04:33:01 +0300

    Add files via upload

--- prakt_2/dinam_2.cpp
+++ prakt_2/dinam_2.cpp
@@ -13,8 +13,24 @@ void handcompl_Array(int num, float** points){
     }
 }
 
+void new_3_Array(int num, float** points, float** n_points){
 
-void rad_center_Array(float** points){
+}
+
+float proverka(float** points, float* r_c, int num){
+    float counter = 0;
+    for(int i; i < num; i++){
+        float l1 = pow((points[i][0] - r_c[0]),2);
+        float l2 = pow((points[i][1] - r_c[1]), 2);
+        if ((l1 + l2) <= pow(r_c[2], 2)){
+            counter++;
+        }
+    }
+    return counter;
+}
+
+
+float* rad_center_Array(float** points){
     float A[N][N] = {
             {2 * points[0][0], 2 * points[0][1], 1},
             {2 * points[1][0], 2 * points[1][1], 1},
@@ -48,10 +64,14 @@ void rad_center_Array(float** points){
         X[i] /= A[i][i];
     }
     float *center = new float[2];
+    float Xc = -(X[0]);
+    float Yc = -(X[1]);
     center[0] = -(X[0]); center[1] = -(X[1]);
-    float R = sqrt(pow(center[0], 2) + pow(center[1], 2) - X[2]);
-    cout << "X и Y центра окружности: "; cout << center[0]; cout << ":";cout << center[1]; cout << endl;
-    cout << "Радиус окружности: "; cout << R;
+    float R = sqrt(pow(Xc, 2) + pow(Yc, 2) - X[2]);
+    float *answer = new float[3];
+    answer[0] = Xc; answer[1] = Yc; answer[2] = R;
+    return answer;
+
 
 }
 
@@ -61,13 +81,49 @@ int main(){
     int num;
     char menu;
     cout << "Введите кол-во точек:"; cin >> num;
-
     float **points = new float*[num];
-    if (num > 3){
-        for(int i = 0; i < num; i++){
-
-        }
-    }
     handcompl_Array(num, points);
-    rad_center_Array(points);
+
+    if (num > 3){
+        float max_R = 0;
+        float *end_res = new float[3];
+        float **res_t = new float*[3];
+        for(int i = 0; i < num; i++){
+            for(int j = i+1; j < num; j++){
+                for(int k = j+1; k < num; k++){
+                    float **sevpoints = new float*[3];
+                    sevpoints[0] = points[i];
+                    sevpoints[1] = points[j];
+                    sevpoints[2] = points[k];
+//                    cout << "X центр = " << rad_center_Array(sevpoints)[0] << endl;
+//                    cout << "Y центр = " << rad_center_Array(sevpoints)[1] << endl;
+                    float R = rad_center_Array(sevpoints)[2];
+//                    cout << "R = " << R << endl;
+                    if (R > max_R){
+                        res_t[0] = points[i];
+                        res_t[1] = points[j];
+                        res_t[2] = points[k];
+                        max_R = R;
+                        end_res[0] = rad_center_Array(sevpoints)[0];
+                        end_res[1] = rad_center_Array(sevpoints)[1];
+                        end_res[2] = proverka(points, rad_center_Array(sevpoints), num);
+                    }
+//                    cout << "Кол-во точек внутри = " << proverka(points, rad_center_Array(sevpoints), num);
+                    delete[] sevpoints;
+                }
+            }
+        }
+        cout << "X центр = " << end_res[0] << endl;
+        cout << "Y центр = " << end_res[1] << endl;
+        cout << "R = " << max_R << endl;
+        cout << "Кол-во точек внутри = " << end_res[2] << endl;
+        cout << "Точки, образующие окр: " << *res_t[0] << " " << *res_t[1] << " " << *res_t[2];
+    }
+    else{
+        cout << "X центр = " << rad_center_Array(points)[0] << endl;
+        cout << "Y центр = " << rad_center_Array(points)[1] << endl;
+        cout << "R = " << rad_center_Array(points)[2] << endl;
+        cout << "Кол-во точек внутри = " << proverka(points, rad_center_Array(points), num);
+    }
+
 }
